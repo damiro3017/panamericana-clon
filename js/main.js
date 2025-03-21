3ec43abef2b876cfdc5c9ebfb6378528d43f1b58
Damiro Gomez <damirogomez74@gmail.com>
2025-03-21 11:16:46 -0500

    feat: Add slider in the hero section. Modification of image paths.

--- js/main.js
+++ js/main.js
@@ -23,3 +23,41 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+
+const slider = document.querySelector(".hero__slider");
+let heroSliderItems = document.querySelectorAll(".hero__slider__item");
+let heroSliderItemLast = heroSliderItems[heroSliderItems.length - 1];
+
+const sliderBtnLeft = document.querySelector(".hero__slider__left-btn");
+const sliderBtnRight = document.querySelector("#hero__slider__right-btn");
+
+// Inserta el último ítem al inicio para el efecto circular
+slider.insertAdjacentElement("afterbegin", heroSliderItemLast);
+
+function moveNext() {
+  let heroSliderItemFirst = document.querySelectorAll(".hero__slider__item")[0];
+  slider.style.marginLeft = "-200%";
+  slider.style.transition = "all 0.3s";
+  setTimeout(function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("beforeend", heroSliderItemFirst);
+    slider.style.marginLeft = "-100%";
+  }, 500);
+}
+
+function movePrev() {
+  let heroSliderItems = document.querySelectorAll(".hero__slider__item");
+  let heroSliderItemLast = heroSliderItems[heroSliderItems.length - 1];
+  slider.style.marginLeft = "0%";
+  slider.style.transition = "all 0.3s";
+  setTimeout(function () {
+    slider.style.transition = "none";
+    slider.insertAdjacentElement("afterbegin", heroSliderItemLast);
+    slider.style.marginLeft = "-100%";
+  }, 1000);
+}
+
+sliderBtnRight.addEventListener("click", moveNext);
+sliderBtnLeft.addEventListener("click", movePrev);
+
+setInterval(moveNext, 7000);
